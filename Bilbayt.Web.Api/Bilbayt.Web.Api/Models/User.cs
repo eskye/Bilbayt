b414bab835ad8db80ebe7278be1f4c0ec1c5ca15
eskye <sunkanmiijatuyi@gmail.com>
2021-07-05 21:26:25 +0100

    Initial commit

---
+++ Bilbayt.Web.Api/Bilbayt.Web.Api/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using AspNetCore.Identity.Mongo.Model;
+
+namespace Bilbayt.Web.Api.Models
+{
+    public class User : MongoUser
+    {
+        public string FullName { get; set; }
+    }
+}
---
+++ Bilbayt.Web.Api/Bilbayt.Web.Api/Services/IMailService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Bilbayt.Web.Api.Services
+{
+    public interface IMailService
+    {
+        Task SendMailAsync();
+    }
+
+    public class MailService : IMailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public MailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendMailAsync()
+        {
+
+              var apiKey = _configuration.GetValue<string>("SendGrid:ApiKey");
+                var client = new SendGridClient(apiKey);
+                var from = new EmailAddress("ijatuyitemi194@gmail.com", "Sunkanmi");
+                var subject = "Sending with SendGrid is Fun";
+                var to = new EmailAddress("sunkanmiijatuyi@gmail.com", "Sunkanmi Ijatuyi");
+                var plainTextContent = "and easy to do anywhere, even with C#";
+                var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
