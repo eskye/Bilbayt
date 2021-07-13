4764eb3d07ae8f53d1ea7d2a4ae5fd8942cb4b11
eskye <sunkanmiijatuyi@gmail.com>
2021-07-13 12:43:58 +0100

    Add cosmosdb client repository

---
+++ src/Bilbayt.IdentityCosmosdb.Infrastructure/CosmosDbContext/Interfaces/IContainerContext.cs
@@ -0,0 +1,16 @@
+﻿using Bilbayt.IdentityCosmosDb.Application.Entities.Base;
+using Microsoft.Azure.Cosmos;
+
+namespace Bilbayt.IdentityCosmosdb.Infrastructure.CosmosDbContext.Interfaces
+{
+    /// <summary>
+    ///  Defines the container level context
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IContainerContext<T> where T : BaseEntity
+    {
+        string ContainerName { get; }
+        string GenerateId(T entity);
+        PartitionKey ResolvePartitionKey(string entityId);
+    }
+}
---
+++ src/Bilbayt.IdentityCosmosdb.Infrastructure/CosmosDbContext/Repository/CosmosDbRepository.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bilbayt.IdentityCosmosDb.Application.Entities.Base;
+using Bilbayt.IdentityCosmosDb.Application.Interfaces.Persistence;
+using Bilbayt.IdentityCosmosdb.Infrastructure.CosmosDbContext.Interfaces;
+using Microsoft.Azure.Cosmos;
+
+namespace Bilbayt.IdentityCosmosdb.Infrastructure.CosmosDbContext.Repository
+{
+    public abstract class CosmosDbRepository<T> : IRepository<T>, IContainerContext<T> where T : BaseEntity
+    {
+        /// <summary>
+        ///     Name of the CosmosDB container
+        /// </summary>
+        public abstract string ContainerName { get; }
+
+        /// <summary>
+        ///     Generate id
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public abstract string GenerateId(T entity);
+
+        /// <summary>
+        ///     Resolve the partition key
+        /// </summary>
+        /// <param name="entityId"></param>
+        /// <returns></returns>
+        public abstract PartitionKey ResolvePartitionKey(string entityId);
+
+        
+
+        /// <summary>
+        ///     Resolve the partition key for the audit record.
+        ///     All entities will share the same audit container,
+        ///     so we can define this method here with virtual default implementation.
+        ///     Audit records for different entities will use different partition key values,
+        ///     so we are not limited to the 20G per logical partition storage limit.
+        /// </summary>
+        /// <param name="entityId"></param>
+        /// <returns></returns>
+        public virtual PartitionKey ResolveAuditPartitionKey(string entityId) => new PartitionKey($"{entityId.Split(':')[0]}:{entityId.Split(':')[1]}");
+
+        /// <summary>
+        ///     Cosmos DB factory
+        /// </summary>
+        private readonly ICosmosDbContainerFactory _cosmosDbContainerFactory;
+
+        /// <summary>
+        ///     Cosmos DB container
+        /// </summary>
+        private readonly Microsoft.Azure.Cosmos.Container _container;
+        /// <summary>
+        ///     Audit container that will store audit log for all entities.
+        /// </summary>
+        private readonly Microsoft.Azure.Cosmos.Container _auditContainer;
+
+        public CosmosDbRepository(ICosmosDbContainerFactory cosmosDbContainerFactory)
+        {
+            this._cosmosDbContainerFactory = cosmosDbContainerFactory ?? throw new ArgumentNullException(nameof(ICosmosDbContainerFactory));
+            this._container = this._cosmosDbContainerFactory.GetContainer(ContainerName)._container;
+            this._auditContainer = this._cosmosDbContainerFactory.GetContainer("Audit")._container;
+        }
+
+        public async Task AddItemAsync(T item)
+        {
+            item.Id = GenerateId(item);
+            await _container.CreateItemAsync<T>(item, ResolvePartitionKey(item.Id));
+        }
+
+        public async Task DeleteItemAsync(string id)
+        {
+            await this._container.DeleteItemAsync<T>(id, ResolvePartitionKey(id));
+        }
+
+        public async Task<T> GetItemAsync(string id)
+        {
+            try
+            {
+                ItemResponse<T> response = await _container.ReadItemAsync<T>(id, ResolvePartitionKey(id));
+                return response.Resource;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+        }
+
+        // Search data using SQL query string
+        // This shows how to use SQL string to read data from Cosmos DB for demonstration purpose.
+        // For production, try to use safer alternatives like Parameterized Query and LINQ if possible.
+        // Using string can expose SQL Injection vulnerability, e.g. select * from c where c.id=1 OR 1=1. 
+        // String can also be hard to work with due to special characters and spaces when advanced querying like search and pagination is required.
+        public async Task<IEnumerable<T>> GetItemsAsync(string queryString)
+        {
+            FeedIterator<T> resultSetIterator = _container.GetItemQueryIterator<T>(new QueryDefinition(queryString));
+            List<T> results = new List<T>();
+            while (resultSetIterator.HasMoreResults)
+            {
+                FeedResponse<T> response = await resultSetIterator.ReadNextAsync();
+
+                results.AddRange(response.ToList());
+            }
+
+            return results;
+        }
+
+         
+        public async Task UpdateItemAsync(string id, T item)
+        {
+             
+            // Update
+            await this._container.UpsertItemAsync<T>(item, ResolvePartitionKey(id));
+        }
+
+         
+
+
+    }
+}
